828218bfeaf327b0eea3bbae6782a39b537b4116
Pascal Geiser <pgeiser@pgeiser.com>
2021-04-22 21:25:15 +0200

    Use local bash-helpers now.

--- scripts/test_iso.sh
+++ scripts/test_iso.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 set -eu
 
-# Fetch bash helpers and source it.
-if [ ! -e .bash_helpers.sh ]; then
-  curl -kSL https://raw.githubusercontent.com/13pgeiser/bash/master/bash_helpers.sh -o .bash_helpers.sh
-fi
-source ./.bash_helpers.sh
+source ./bash-helpers/helpers.sh
 
 echo "Starting QEMU"
-launch_qemu tcp::2222-:22 4G buster-standard.iso
+qemu_launch tcp::2222-:22 4G buster-standard.iso
 echo "Waiting for SSH connection"
-wait_for_ssh localadmin@localhost 2222
+qemu_wait_for_ssh localadmin@localhost 2222
 echo "Copying SSH keys"
-copy_ssh_keys localadmin@localhost 2222
+qemu_copy_ssh_keys localadmin@localhost 2222
 echo "Listing slash"
 ssh -p 2222 localadmin@localhost "sudo ls /"
 echo "Calling shutdown!"
